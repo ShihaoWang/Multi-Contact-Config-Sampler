8341bc27b9f5b11c0876635711a0cee8caae9935
ShihaoWang <sw309@duke.edu>
2020-05-29 10:04:54 -0500

    Commit for Backup

---
+++ build/CMakeFiles/MyApp.dir/DependInfo.cmake
@@ -0,0 +1,59 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/motion/Desktop/Multi-Contact-Config-Sampler/src/ContactInfoLoader.cpp" "/home/motion/Desktop/Multi-Contact-Config-Sampler/build/CMakeFiles/MyApp.dir/src/ContactInfoLoader.cpp.o"
+  "/home/motion/Desktop/Multi-Contact-Config-Sampler/src/ConvexPolyhedron.cpp" "/home/motion/Desktop/Multi-Contact-Config-Sampler/build/CMakeFiles/MyApp.dir/src/ConvexPolyhedron.cpp.o"
+  "/home/motion/Desktop/Multi-Contact-Config-Sampler/src/EnviGeometry.cpp" "/home/motion/Desktop/Multi-Contact-Config-Sampler/build/CMakeFiles/MyApp.dir/src/EnviGeometry.cpp.o"
+  "/home/motion/Desktop/Multi-Contact-Config-Sampler/src/InitialConfigurationOptimization.cpp" "/home/motion/Desktop/Multi-Contact-Config-Sampler/build/CMakeFiles/MyApp.dir/src/InitialConfigurationOptimization.cpp.o"
+  "/home/motion/Desktop/Multi-Contact-Config-Sampler/src/ReachabilityMap.cpp" "/home/motion/Desktop/Multi-Contact-Config-Sampler/build/CMakeFiles/MyApp.dir/src/ReachabilityMap.cpp.o"
+  "/home/motion/Desktop/Multi-Contact-Config-Sampler/src/RobotStateFiler.cpp" "/home/motion/Desktop/Multi-Contact-Config-Sampler/build/CMakeFiles/MyApp.dir/src/RobotStateFiler.cpp.o"
+  "/home/motion/Desktop/Multi-Contact-Config-Sampler/src/RobotUtils.cpp" "/home/motion/Desktop/Multi-Contact-Config-Sampler/build/CMakeFiles/MyApp.dir/src/RobotUtils.cpp.o"
+  "/home/motion/Desktop/Multi-Contact-Config-Sampler/src/Sampler.cpp" "/home/motion/Desktop/Multi-Contact-Config-Sampler/build/CMakeFiles/MyApp.dir/src/Sampler.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ASSIMP_MAJOR_VERSION=3"
+  "ENABLE_NON_DETERMINISTIC_PARALLELISM"
+  "HAVE_ASSIMP=1"
+  "HAVE_CURL=1"
+  "HAVE_FREE_IMAGE=1"
+  "HAVE_GLEW=1"
+  "HAVE_GLPK=1"
+  "HAVE_GLUI=1"
+  "HAVE_GLUT=1"
+  "HAVE_LOG4CXX=1"
+  "HAVE_PANGOLIN"
+  "HAVE_ROS=1"
+  "HAVE_TINYXML=1"
+  "LOG4CXX_STATIC"
+  "TIXML_USE_STL"
+  "USE_CPP_THREADS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/motion/Klampt/Cpp/Dependencies/glui-2.36/src/include"
+  "/home/motion/Klampt/Cpp/Dependencies"
+  "/home/motion/Klampt/Cpp/Dependencies/ode-0.14/include"
+  "/opt/ros/kinetic/include"
+  "/home/motion/Klampt/include"
+  "/home/motion/Klampt/Cpp"
+  "/home/motion/Klampt/Cpp/Dependencies/KrisLibrary"
+  "/home/motion/snopt7/include"
+  "/home/motion/eigen-3.3.5"
+  "/home/motion/cilantro/include"
+  "/home/motion/Pangolin/include"
+  "/home/motion/Pangolin/build/src/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/MyApp.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyApp.dir/src/Sampler.cpp.o"
+  "CMakeFiles/MyApp.dir/src/ContactInfoLoader.cpp.o"
+  "CMakeFiles/MyApp.dir/src/EnviGeometry.cpp.o"
+  "CMakeFiles/MyApp.dir/src/RobotStateFiler.cpp.o"
+  "CMakeFiles/MyApp.dir/src/RobotUtils.cpp.o"
+  "CMakeFiles/MyApp.dir/src/InitialConfigurationOptimization.cpp.o"
+  "CMakeFiles/MyApp.dir/src/ReachabilityMap.cpp.o"
+  "CMakeFiles/MyApp.dir/src/ConvexPolyhedron.cpp.o"
+  "MyApp.pdb"
+  "MyApp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
